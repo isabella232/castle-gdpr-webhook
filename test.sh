aa8f9a6d296a6da8b73a0a83a72968730158cf08
Ola Nordstrom <ola.nordstrom@optimizely.com>
2020-01-07 14:59:18 -0800

    removed test.sh since it\'s misleading

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#curl -v -X POST "https://a9jh25kbsb.execute-api.us-west-2.amazonaws.com/test/helloworld"
-#curl -v -X GET "https://a9jh25kbsb.execute-api.us-west-2.amazonaws.com/test/helloworld"
-
-#curl -v -X GET "https://it698mmchk.execute-api.us-west-2.amazonaws.com/test/hellowworld"
-curl -v -X GET "https://2skl70ci2g.execute-api.us-west-2.amazonaws.com/test/test"
-curl -v -X GET "https://2skl70ci2g.execute-api.us-west-2.amazonaws.com/test/test?some=weewaa"
